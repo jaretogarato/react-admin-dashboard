97a95a630c0cb5b5ac9478b33abf8288a3ea1b0b
Jared Gallardo <jaredgg@gmail.com>
2022-12-20 12:41:50 -0700

    add pie chart component and page

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Contacts from './scenes/contacts'
 import Invoices from './scenes/invoices'
 import Form from './scenes/form'
 import Bar from './scenes/bar'
+import Pie from './scenes/pie'
 // import Line from './scenes/line'
-// import Pie from './scenes/pie'
 // import FAQ from './scenes/faq'
 // import Calendar from './scenes/calendar'
 // import Geography from './scenes/geography'
@@ -33,7 +33,7 @@ function App() {
 							<Route path='/invoices' element={<Invoices />} />
 							<Route path='/form' element={<Form />} />
 							<Route path='/bar' element={<Bar />} />
-							{/* <Route path='/pie' element={<Pie />} /> */}
+							<Route path='/pie' element={<Pie />} />
 							{/* <Route path='/line' element={<Line />} /> */}
 							{/* <Route path='/faq' element={<FAQ />} /> */}
 							{/* <Route path='/geography' element={<Geography />} /> */}
